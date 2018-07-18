0379b32d795e8cee43e6832a2f1e25c074c8c976
Jarvis-SiegeLion <dengjwsjtu@163.com>
2018-07-18 21:03:22 +0800

    Update bayes.py

--- bayes.py
+++ bayes.py
@@ -1,1 +1,79 @@
+'''
+    This is an example of Naive Bayes to identify whether a sentence is abusive or not from "Machine Learning In Action".
+'''
+
+import numpy as np
+
+def loadDataSet():
+    postingList = [
+        ['my','dog','has','flea','problems','help','please'],
+        ['maybe','not','take','him','to','dog','park','stupid'],
+        ['my','dalmation','is','so','cute','I','love','him'],
+        ['stop','posting','stupid','worthless','garbage'],
+        ['mr','licks','ate','my','steak','how','to','stop','him'],
+        ['quit','buying','worthless','dog','food','stupid'],
+    ]
+    classVec = [0,1,0,1,0,1]
+    return postingList,classVec
+
+#create vocabulary list
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+#convert vocab list to vector
+def setOfWords2Vec(vocabList,inputSet):
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:   print("the word: %s is not in my Vocabulary!" % word)
+    return returnVec
+
+listOPosts,listClasses = loadDataSet()
+myVocabList = createVocabList(listOPosts)
+
+#train the dataset
+def trainNB0(trainMatrix,trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory)/float(numTrainDocs)
+    p0Num = np.ones(numWords);p1Num = np.ones(numWords)
+    p0Denom = 2.0;p1Denom = 2.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    p1Vect = np.log(p1Num/p1Denom)
+    p0Vect = np.log(p0Num/p0Denom)
+    return p0Vect,p1Vect,pAbusive
+
+#classification function
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    p1 = sum(vec2Classify * p1Vec) + np.log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
+    if p1>p0:
+        return 1
+    else:
+        return 0
+
+# test function
+def testingNB():
+    listOposts,listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+    p0V,p1V,pAb = trainNB0(np.array(trainMat),np.array(listClasses))
+    testEntry = ['love','my','dalmation']
+    thisDoc = np.array(setOfWords2Vec(myVocabList,testEntry))
+    print(testEntry,'classified as:',classifyNB(thisDoc,p0V,p1V,pAb))
+    testEntry = ['stupid','garbage']
+    thisDoc = np.array(setOfWords2Vec(myVocabList,testEntry))
+    print(testEntry,'classified as:',classifyNB(thisDoc,p0V,p1V,pAb))
 
